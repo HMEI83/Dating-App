517e5261938d1e238680db300f2e946e38c833df
HMEI83 <101045819+HMEI83@users.noreply.github.com>
2023-02-07 15:28:03 +1100

    Persisting the login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
@@ -11,13 +13,25 @@ export class AppComponent implements OnInit{
   title = 'Dating App';
   users: any;
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private accountService: AccountService){}
   ngOnInit(): void {
+    this.getUser();
+    this.setCurrentUser();
+  }
+
+  getUser() {
     this.http.get('http://localhost:5090/api/users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
       complete: () => console.log('Request has complete!')
     })
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 
 }
