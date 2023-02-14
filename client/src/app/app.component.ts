3e9e944b8aff3e44ae5edd10b311fe885e773815
HMEI83 <101045819+HMEI83@users.noreply.github.com>
2023-02-14 15:25:49 +1100

    Hooking up the register method to the service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
-
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,20 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'Dating App';
-  users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService){}
+  constructor(private accountService: AccountService){}
+
   ngOnInit(): void {
-    this.getUser();
     this.setCurrentUser();
-  }
-
-  getUser() {
-    this.http.get('http://localhost:5090/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has complete!')
-    })
   }
 
   setCurrentUser() {
